3be67529f4b32607120192eef181c8f64936d914
okHassan <freelancerhassan3@gmail.com>
2023-12-22 16:57:37 +0600

    devmode-on

---
+++ src/Construction.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TailwindUnderDevelopment = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold">Under Development</h1>
+        </div>
+    );
+}
+
+export default TailwindUnderDevelopment;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import TailwindUnderDevelopment from './Construction';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <App />
+        <TailwindUnderDevelopment />
     </BrowserRouter>
 );
 
