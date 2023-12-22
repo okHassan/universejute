ade9ff4115d0edf5c95e258b7cfb22a8b7094243
okHassan <freelancerhassan3@gmail.com>
2023-12-22 17:11:52 +0600

    devmode-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Helmet } from "react-helmet-async";
+
 import Home from './Pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Product from './Pages/Product';
 import Contact from './Pages/Contact';
 
 function App() {
+
+    useEffect(() => {
+        alert('This site is under development. Please check back later. Thank you. :)')
+    }, [])
+
     return (
         <React.Fragment>
+            <Helmet>
+                <title>Design+Code - Learn to design and code React and Swift apps</title>
+                <meta
+                    name="description"
+                    content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
+                />;
+            </Helmet>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/product' element={<Product />} />
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import TailwindUnderDevelopment from './Construction';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <TailwindUnderDevelopment />
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
     </BrowserRouter>
 );
 
